a88d9e0641e7e19247139ccc93db55d470dfb374
blakeparkinson <blake@edmodo.com>
2014-12-22 23:07:52 -0800

    left sidebar

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 
     // Populate the user table on initial page load
     populateTable();
+    
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 
 });
 
@@ -21,6 +23,7 @@ function populateTable() {
     // jQuery AJAX call for JSON
     $.getJSON( '/users/userlist', function( data ) {
 
+        userListData = data;
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
@@ -34,3 +37,27 @@ function populateTable() {
         $('#userList table tbody').html(tableContent);
     });
 };
+
+// Show User Info
+function showUserInfo(event) {
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    var thisUserName = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+}
+
+
