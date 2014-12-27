fb19f0ffad9099ff2781033b112983a59fa87407
blakeparkinson <blake@edmodo.com>
2014-12-27 00:21:44 -0600

    more

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,16 @@ router.get('/userlist', function(req, res) {
     });
 });
 
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    db.collection('userlist').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
 module.exports = router;
