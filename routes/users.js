d80cbfa3bb33e6de2c58ba163215ef878ce45cf8
blakeparkinson <blake@edmodo.com>
2014-12-27 00:53:08 -0600

    deleting users

--- routes/users.js
+++ routes/users.js
@@ -23,4 +23,16 @@ router.post('/adduser', function(req, res) {
     });
 });
 
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('userlist').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+
 module.exports = router;
